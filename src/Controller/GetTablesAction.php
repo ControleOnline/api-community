88be45cbb2168793665c39c7f763eff363560ea2
Luiz Kim <luiz.kim@controleonline.com>
2023-12-19 22:41:21 -0300

    Multi Tenency

--- src/Controller/GetTablesAction.php
+++ src/Controller/GetTablesAction.php
@@ -46,7 +46,7 @@ class GetTablesAction
 
       $nqu = $this->manager->createNativeQuery($sql, $rsm);
 
-      $nqu->setParameter('domain', $request->get('domain', null));
+      $nqu->setParameter('domain', $request->get('app-domain', null));
 
       $taxNames = $nqu->getArrayResult();
 
