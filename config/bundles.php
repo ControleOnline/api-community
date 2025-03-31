33b0b6616a5ef2199c0ec0710abc4bd8a7b86d62
Luiz Kim <luiz.kim@controleonline.com>
2025-03-31 14:27:58 -0300

    Upgrade Symfony

--- config/bundles.php
+++ config/bundles.php
@@ -5,12 +5,10 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    Gos\Bundle\PubSubRouterBundle\GosPubSubRouterBundle::class => ['all' => true],
-    Gos\Bundle\WebSocketBundle\GosWebSocketBundle::class => ['all' => true],
 ];
