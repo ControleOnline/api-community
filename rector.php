e1d6fd006a753528a5f8e1ed972a2ffc0cf848c1
Luiz Kim <luiz.kim@controleonline.com>
2025-04-12 21:43:44 -0300

    Update

--- rector.php
+++ rector.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Rector\GroupsToContextRector;
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Doctrine\Set\DoctrineSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    // Definir os diretórios de entidades em todos os módulos
     $rectorConfig->paths([
         __DIR__ . '/modules/controleonline/multi-tenancy/src/Entity',
         __DIR__ . '/modules/controleonline/products/src/Entity',
@@ -23,18 +23,19 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/modules/controleonline/accounting/src/Entity',
     ]);
 
-    // Aplicar regras para converter anotações Doctrine para atributos
     $rectorConfig->sets([
         DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ]);
 
-    // Incluir regras para PHP 8.3 (otimizado para sua versão)
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_83,
     ]);
 
-    // Ignorar arquivos ou pastas específicas, se necessário
+    //$rectorConfig->rule(GroupsToContextRector::class);
+
     $rectorConfig->skip([
         // Exemplo: __DIR__ . '/modules/controleonline/some-module/src/Entity/SpecificEntity.php',
     ]);
+
+    $rectorConfig->importNames();
 };
