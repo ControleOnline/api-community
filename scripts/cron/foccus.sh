6764812becd09824a049ec66a4604bb06e442ab1
Luiz Kim <luiz.kim@controleonline.com>
2025-05-24 21:05:59 -0300

    Add Connections Table

--- scripts/cron/foccus.sh
+++
@@ -1,7 +0,0 @@
-cd ~/sistemas/api
-/usr/local/bin/php bin/console app:fix pending-logistic
-
-/usr/local/bin/php bin/console app:order-notifier create_logistic_invoice
-
-
-/usr/local/bin/php bin/console app:fix order-status-by-contract
