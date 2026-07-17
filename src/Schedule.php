f50d234d79b38533882aa62cbe3e127e99e3ae57
Luiz Kim <luiz.kim@controleonline.com>
2026-07-17 11:23:30 -0300

    feat: implementar agendamento de tarefas com suporte a mensagens recorrentes

--- src/Schedule.php
+++ src/Schedule.php
@@ -2,6 +2,11 @@
 
 namespace App;
 
+use ControleOnline\Service\CronJobService;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Console\Messenger\RunCommandMessage;
+use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\Schedule as SymfonySchedule;
 use Symfony\Component\Scheduler\ScheduleProviderInterface;
@@ -10,19 +15,47 @@ use Symfony\Contracts\Cache\CacheInterface;
 #[AsSchedule]
 class Schedule implements ScheduleProviderInterface
 {
+    private ?SymfonySchedule $schedule = null;
+
     public function __construct(
-        private CacheInterface $cache,
+        #[Autowire(service: 'cache.scheduler')]
+        private CacheInterface $schedulerCache,
+        private LockFactory $lockFactory,
+        private CronJobService $cronJobService,
     ) {
     }
 
     public function getSchedule(): SymfonySchedule
     {
-        return (new SymfonySchedule())
-            ->stateful($this->cache) // ensure missed tasks are executed
-            ->processOnlyLastMissedRun(true) // ensure only last missed task is run
+        if ($this->schedule instanceof SymfonySchedule) {
+            return $this->schedule;
+        }
 
-            // add your own tasks here
-            // see https://symfony.com/doc/current/scheduler.html#attaching-recurring-messages-to-a-schedule
-        ;
+        $schedule = (new SymfonySchedule())
+            ->lock($this->lockFactory->createLock('scheduler:cron-jobs'))
+            ->stateful($this->schedulerCache)
+            ->processOnlyLastMissedRun(true);
+
+        foreach ($this->cronJobService->getConfiguredJobs() as $job) {
+            if (
+                !($job['enabled'] ?? false)
+                || !($job['isValid'] ?? false)
+                || trim((string) ($job['key'] ?? '')) === ''
+                || trim((string) ($job['command'] ?? '')) === ''
+            ) {
+                continue;
+            }
+
+            $schedule->add(
+                RecurringMessage::cron(
+                    (string) $job['cronExpression'],
+                    new RunCommandMessage(
+                        sprintf('app:cron:run-job %s', (string) $job['key'])
+                    )
+                )
+            );
+        }
+
+        return $this->schedule = $schedule;
     }
 }
