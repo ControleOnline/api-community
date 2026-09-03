72ea8b35124f596e0dc001fc13a341a106066a5f
Luiz Kim <luiz.kim@controleonline.com>
2026-09-03 16:06:21 -0300

    chore: pin rotating tenant consumer

--- config/phpunit-bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require __DIR__.'/bootstrap.php';
-require_once __DIR__.'/test-reporting.php';
-
-ensureTestReportDirectory(dirname(__DIR__).'/var/tests/phpunit/backend');
--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install nginx certbot python3-certbot-nginx openjdk-21-jre-headless -y
-sh swap.sh
-sh node.sh
-sh firewall.sh
-
-
-#sudo certbot --nginx -d mydomain.com
